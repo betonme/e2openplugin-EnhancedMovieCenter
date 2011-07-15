e2565daaa140fc76240c5af74c9282c11e7ba4b1
betonme <betonme@398de0b3-2755-4f39-ad97-b6641548fef7>
2011-07-15 15:20:26 +0000

    Test for Bug #7 ShortDescription mit E2 converter auch bei nicht *.ts Dateien anzeigen
    git-svn-id: svn://85.10.198.214/trunk@122 398de0b3-2755-4f39-ad97-b6641548fef7

--- Converter/EMCMovieInfo.py
+++ Converter/EMCMovieInfo.py
@@ -53,35 +53,44 @@ class EMCMovieInfo(MovieInfo):
 			if self.type == self.MOVIE_SHORT_DESCRIPTION:
 				event = self.source.event
 				shortdesc = event and info and service and info.getInfoString(service, iServiceInformation.sDescription)
+				print "EMC shortdesc1 " + str(shortdesc)
 				if not shortdesc:
 					shortdesc = event and event.getShortDescription()
+				print "EMC shortdesc2 " + str(shortdesc)
 				if not shortdesc:
 					self.eit = service and EitList(service)
 					shortdesc = self.eit and self.eit.getEitDescription()
+				print "EMC shortdesc3 " + str(shortdesc)
 				if not shortdesc:
 					#self.meta = service and MetaList(service, borg=True)
 					#shortdesc = self.meta and self.meta.getMetaDescription()
 					# Test only
 					rec_ref_str = info and info.getInfoString(service, iServiceInformation.sServiceref)
 					shortdesc = rec_ref_str and ServiceReference(rec_ref_str).getServiceName()
+				print "EMC shortdesc4 " + str(shortdesc)
 				return shortdesc or ""
 						
 			elif self.type == self.MOVIE_META_DESCRIPTION:
 				extdesc = info and service and info.getInfoString(service, iServiceInformation.sDescription)
+				print "EMC extdesc1 " + str(extdesc)
 				if not extdesc:
 					extdesc = event and event.getExtendedDescription()
+				print "EMC extdesc2 " + str(extdesc)
 				if not extdesc:
 					#print "EMC EIT extdesc Meta"
 					self.meta = service and MetaList(service, borg=True)
 					extdesc = self.meta and self.meta.getMetaDescription()
+				print "EMC extdesc3 " + str(extdesc)
 				if not extdesc:
 					#print "EMC EIT extdesc Eit"
 					self.eit = service and EitList(service)
 					extdesc = self.eit and self.eit.getEitDescription()
+				print "EMC extdesc4 " + str(extdesc)
 				if not extdesc:
 					# Test only
 					rec_ref_str = info and info.getInfoString(service, iServiceInformation.sServiceref)
 					extdesc = rec_ref_str and ServiceReference(rec_ref_str).getServiceName()
+				print "EMC extdesc5 " + str(extdesc)
 				return extdesc or ""
 				
 			elif self.type == self.MOVIE_REC_SERVICE_NAME:
@@ -92,6 +101,17 @@ class EMCMovieInfo(MovieInfo):
 				filesize = info.getInfoObject(service, iServiceInformation.sFileSize)
 				if filesize is not None:
 					return "%d MB" % (filesize / (1024*1024))
+				
+				# Temp only
+#				if isinstance(service, iPlayableServicePtr):
+#					info = service and service.info()
+#					ref = None
+#				else: # reference
+#					info = service and self.source and self.source.info
+#					ref = service
+#				name = ref and info and info.getName(ref)
+#				if name is None:
+#					name = info.getName()
 				
 		except Exception, e:
 			print "[EMCMI] getText exception:" + str(e)
