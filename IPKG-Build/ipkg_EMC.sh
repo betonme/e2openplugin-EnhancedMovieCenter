0bb15414d6162ce4a23910b296a505cd7a3333b5
Swiss-MAD <Swiss-MAD@398de0b3-2755-4f39-ad97-b6641548fef7>
2011-08-16 23:04:08 +0000

    - Add some more setup helptext - Update german po - Add first try english po with helptext - Add IPKG Build scripts
    git-svn-id: svn://85.10.198.214/trunk@145 398de0b3-2755-4f39-ad97-b6641548fef7

---
+++ IPKG-Build/ipkg_EMC.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo "Kopiere Plugin Enhanced Movie Center nach /tmp/temp/"
+mkdir -p /tmp/temp/usr/lib/enigma2/python/Plugins/Extensions/
+cp -r /usr/lib/enigma2/python/Plugins/Extensions/EnhancedMovieCenter/ /tmp/temp/usr/lib/enigma2/python/Plugins/Extensions/
+
+echo "Kopiere Converter (nur *.pyo) nach /tmp/temp/"
+mkdir -p /tmp/temp/usr/lib/enigma2/python/Components/Converter/
+cp -r /usr/lib/enigma2/python/Components/Converter/EMCClockToText.pyo /tmp/temp/usr/lib/enigma2/python/Components/Converter/
+cp -r /usr/lib/enigma2/python/Components/Converter/EMCEventName.pyo /tmp/temp/usr/lib/enigma2/python/Components/Converter/
+cp -r /usr/lib/enigma2/python/Components/Converter/EMCServicePosition.pyo /tmp/temp/usr/lib/enigma2/python/Components/Converter/
+cp -r /usr/lib/enigma2/python/Components/Converter/EMCServiceTime.pyo /tmp/temp/usr/lib/enigma2/python/Components/Converter/
+cp -r /usr/lib/enigma2/python/Components/Converter/EMCMovieInfo.pyo /tmp/temp/usr/lib/enigma2/python/Components/Converter/
+
+echo "Kopiere Config Dateien aus /etc/enigma2/ nach /tmp/temp/"
+mkdir -p /tmp/temp/etc/enigma2/
+cp -r /etc/enigma2/emc-hide.cfg /tmp/temp/etc/enigma2/
+cp -r /etc/enigma2/emc-noscan.cfg /tmp/temp/etc/enigma2/
+
+echo "Lösche alle *.py unter /tmp/temp/"
+rm -r /tmp/temp/usr/lib/enigma2/python/Plugins/Extensions/EnhancedMovieCenter/*.py
+
+echo "Kopiere control Daten nach /tmp/temp/"
+cp -r /media/ba/ipk/EMC/CONTROL/ /tmp/temp/
+
+echo "IPKG erstellung läuft...."
+ipkg-build.sh /tmp/temp/ /tmp
+echo "IPKG erstellt unter /tmp/"
