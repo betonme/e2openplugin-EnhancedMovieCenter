e9e31b3a38c0683bd37c1095de2dc64eab7625a1
Coolman <Coolman@398de0b3-2755-4f39-ad97-b6641548fef7>
2011-09-10 08:49:31 +0000

    BUG Fix
    git-svn-id: svn://85.10.198.214/trunk@245 398de0b3-2755-4f39-ad97-b6641548fef7

--- ServiceSupport.py
+++ ServiceSupport.py
@@ -1,25 +1,23 @@
-'''
-Copyright (C) 2011 betonme
-
-In case of reuse of this source code please do not remove this copyright.
-
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	For more information on the GNU General Public License see:
-	<http://www.gnu.org/licenses/>.
-
-For example, if you distribute copies of such a program, whether gratis or for a fee, you 
-must pass on to the recipients the same freedoms that you received. You must make sure 
-that they, too, receive or can get the source code. And you must show them these terms so they know their rights.
-'''
+#!/usr/bin/python
+# encoding: utf-8
+#
+# Copyright (C) 2011 betonme
+#
+# In case of reuse of this source code please do not remove this copyright.
+#
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	For more information on the GNU General Public License see:
+#	<http://www.gnu.org/licenses/>.
+#
 
 import os
 import struct
