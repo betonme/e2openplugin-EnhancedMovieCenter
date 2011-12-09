ac779f7cc727e6a22d37aa0a430a0fa352969efc
betonme <betonme@398de0b3-2755-4f39-ad97-b6641548fef7>
2011-12-09 06:41:29 +0000

    Bugfix: Setup option Preview Delay won't be hidden if preview is off, because options will only be hidden if the parent option is logical False = "", 0, (), [], False
    git-svn-id: svn://85.10.198.214/trunk@472 398de0b3-2755-4f39-ad97-b6641548fef7

--- MovieSelection.py
+++ MovieSelection.py
@@ -77,7 +77,7 @@ class SelectionEventInfo:
 		#self.noCoverPixmap = None
 
 	def initPig(self):
-		if config.EMC.movie_pig.value == "No":
+		if config.EMC.movie_pig.value == "":
 			self["Cover"].hide()
 			self["Video"].show()
 		elif config.EMC.movie_pig.value == "C":
