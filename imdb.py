7cbf5ca55692e2b0ab03faab66b3e06ab9c20d26
dirtylion <dirtylion@398de0b3-2755-4f39-ad97-b6641548fef7>
2012-01-13 18:28:23 +0000

    - diverse bugs fixed. - diverse layout bugs fixed. - mit Rot kann man jetzt die covers löschen. - screen vergrössert. - besprochende sachen aus dem forum wurden abgearbeitet.
    git-svn-id: svn://85.10.198.214/trunk@509 398de0b3-2755-4f39-ad97-b6641548fef7

--- imdb.py
+++ imdb.py
@@ -39,21 +39,22 @@ class imdblist(MenuList):
 
 def imdb_show(title, pp, elapsed, genre, search_title):
 	res = [ (title, pp, elapsed, genre, search_title) ]
-	elapsed = "%s ms." % elapsed
-	res.append(MultiContentEntryText(pos=(0, 0), size=(450, 24), font=4, text=search_title, flags=RT_HALIGN_LEFT))
-	res.append(MultiContentEntryText(pos=(460, 0), size=(100, 24), font=4, text=elapsed, flags=RT_HALIGN_LEFT))
+	if not elapsed == "Exist already":
+		elapsed = "%s ms." % elapsed
+	res.append(MultiContentEntryText(pos=(0, 0), size=(510, 24), font=4, text=search_title, flags=RT_HALIGN_LEFT))
+	res.append(MultiContentEntryText(pos=(500, 0), size=(172, 24), font=4, text=elapsed, flags=RT_HALIGN_LEFT))
 	return res
 
 class imdbscan(Screen):
 	if getDesktop(0).size().width() == 1280:
 		skin = """
-			<screen position="center,center" size="800,550" title="EMC iMDB">
-				<widget name="menulist" position="220,100" size="572,450" scrollbarMode="showOnDemand" transparent="1"/>
-				<widget name="info" position="0,0" size="800,24" zPosition="0" font="Regular;21" halign="center" valign="center" transparent="1" foregroundColor="#ffffff" backgroundColor="black"/>
+			<screen position="center,center" size="900,560" title="EMC iMDB">
+				<widget name="menulist" position="220,100" size="672,408" scrollbarMode="showOnDemand" transparent="1"/>
+				<widget name="info" position="0,0" size="900,24" zPosition="0" font="Regular;21" halign="center" valign="center" transparent="1" foregroundColor="#ffffff" backgroundColor="black"/>
 				<widget name="poster" zPosition="2" position="20,40" size="170,230" alphatest="blend" />
-				<widget name="m_info" position="200,40" size="400,24" zPosition="0" font="Regular;21" halign="center" valign="center" transparent="1" foregroundColor="#ffffff" backgroundColor="black"/>
-				<widget name="genre" position="200,64" size="400,24" zPosition="0" font="Regular;21" halign="center" valign="center" transparent="1" foregroundColor="#ffffff" backgroundColor="black"/>
-				<widget name="done_msg" position="0,500" size="790,24" zPosition="0" font="Regular;21" halign="center" valign="center" transparent="1" foregroundColor="#ffffff" backgroundColor="black"/>
+				<widget name="m_info" position="200,40" size="700,24" zPosition="0" font="Regular;21" halign="center" valign="center" transparent="1" foregroundColor="#ffffff" backgroundColor="black"/>
+				<widget name="genre" position="200,64" size="700,24" zPosition="0" font="Regular;21" halign="center" valign="center" transparent="1" foregroundColor="#ffffff" backgroundColor="black"/>
+				<widget name="done_msg" position="0,512" size="840,48" zPosition="0" font="Regular;21" halign="center" valign="center" transparent="1" foregroundColor="#ffffff" backgroundColor="black"/>
 				<widget name="ButtonGreen" pixmap="skin_default/buttons/key_green.png" position="21,300" zPosition="10" size="35,25" transparent="1" alphatest="on" />
 				<widget name="ButtonGreenText" position="65,300" size="300,22" valign="center" halign="left" zPosition="10" font="Regular;20" transparent="1" />
 			</screen>"""
@@ -66,11 +67,14 @@ class imdbscan(Screen):
 	def __init__(self, session, data):
 		Screen.__init__(self, session, data)
 		self.m_list = data
-		self["actions"] = ActionMap(["SetupActions", "ColorActions"],
-		#self["actions"] = HelpableActionMap(self, "sjActions",
+		#self["actions"] = ActionMap(["SetupActions", "ColorActions"],
+		#self["actions"] = HelpableActionMap(self, "EMCimdb",
+		self["actions"] = HelpableActionMap(self, "EMCimdb",
 		{
-			"cancel":		self.exit,
-			"green":		self.imdb,
+			"EMCEXIT":		self.exit,
+			"EMCOK":                self.ok,
+			"EMCGreen":		self.imdb,
+			"EMCRed":		self.red,
 
 		}, -1)
 
@@ -85,6 +89,9 @@ class imdbscan(Screen):
 		self["done_msg"] = Label("")
 		self["info"].setText("Um die Suche zu starten, drück bitte die Gruene-Taste")
 
+		self.no_image_poster = "/usr/lib/enigma2/python/Plugins/Extensions/EnhancedMovieCenter/img/no_poster.jpg"
+		#self.poster_resize("/usr/lib/enigma2/python/Plugins/Extensions/EnhancedMovieCenter/img/no_poster.jpg")
+
 		self["menulist"].onSelectionChanged.append(self.showInfo)
 		#self.onLayoutFinish.append(self.showInfo)
 		self.check = "false"
@@ -94,22 +101,24 @@ class imdbscan(Screen):
 		if self.check == "true":
 			m_title = self["menulist"].getCurrent()[0][0]
 			m_poster_path = self["menulist"].getCurrent()[0][1]
-			m_real_title = self["menulist"].getCurrent()[0][2]
+			#m_real_title = self["menulist"].getCurrent()[0][2]
 			m_genre = self["menulist"].getCurrent()[0][3]
-			m_o_title = self["menulist"].getCurrent()[0][4]
-			self.poster_resize(m_poster_path)
+			#m_o_title = self["menulist"].getCurrent()[0][4]
+			if os.path.exists(m_poster_path):
+				self.poster_resize(m_poster_path)
+			else:
+				self.poster_resize(self.no_image_poster)
+				
 			self["m_info"].setText(m_title)
 			self["genre"].setText(m_genre)
 
 	### Liste umwandeln und title weitergeben zur imdb suche ###
 	def imdb(self):
-		#klon = self["list"].list
-		#print klon
 		self.counter = 0
 		self.t_elapsed = 0
 		self.menulist = []
-		#m_list = [["superman", "/home/root/", "saw.ts"], ["alice im wunderland", "/home/root/", "saw.ts"], ["greek", "/home/root/", "saw.ts"], ["saw 4", "/home/root/", "saw.ts"], ["saw", "/home/root/", "saw.ts"], ["sex and the city tv", "/tmp/", "sex and the city.ts"], ["final destination", "/tmp/", "final destination.st"], ["star wars", "/tmp/", "star wars.ts"], ["saw 2", "/home/root/", "saw 2.ts"], ["Terminator", "/home/root/", "terminator.ts"], ["Mad Men", "/home/root/", "Mad Men.ts"]]
 		self.count_movies = len(self.m_list)
+		self.poster_resize(self.no_image_poster)
 
 		for each in self.m_list:
         		self.name = each[0].replace(' ','.')
@@ -135,43 +144,48 @@ class imdbscan(Screen):
 		
 		count = "Film: " + str(self.counter) + " von " + str(self.count_movies) + "       Took: " + str(elapsed) + " ms" + "       Total Time: " + str(self.t_elapsed) + " ms"
 
-		print "drinnen:", path
 		### Parsing infos from data ###
+
+		movie_name = re.findall('"Title":"(.*?)"."Year":"(.*?)"', data)
+                movie_title = str(movie_name[0][0]) + " " + str(movie_name[0][1])
+
 		poster_url = re.findall('"Poster":"(.*?)"', data)
 		if poster_url[0] == "N/A":
 			print "N/A"
-			os.system("cp /usr/lib/enigma2/python/Plugins/Extensions/EnhancedMovieCenter/img/no_poster.jpg " + "'" + path + "'")
+			#os.system("cp /usr/lib/enigma2/python/Plugins/Extensions/EnhancedMovieCenter/img/no_poster.jpg " + "'" + path + "'")
 			#poster_path = "/tmp/" + "no_poster.jpg"
-		else:
-			print "NOOOOOT N/A"
-			#poster_jpg = poster_url[0].replace('http://ia.media-imdb.com/images/M/','')
-
-		movie_name = re.findall('"Title":"(.*?)"."Year":"(.*?)"', data)
-		movie_title = str(movie_name[0][0]) + " " + str(movie_name[0][1])
-
-		got_genre = re.findall('"Genre":"(.*?)"', data)
-		genre = "(%s)" % got_genre[0]
-
-		### Downloading Cover mit file exist check ###
-		if not os.path.exists(path):
-			print "EMC iMDB: Download", poster_url[0]
-			urllib._urlopener = AppURLopener()
-			urllib.urlretrieve(poster_url[0], path)
-			urllib.urlcleanup()
-
-		### Uebergabe zum screen ###
-		if os.path.exists(path):
 			self["info"].setText(count)
-			self["m_info"].setText(movie_title)
-			self["genre"].setText(genre)
-			self.menulist.append(imdb_show(movie_title, path, str(elapsed), genre, search_title))
+                        self["m_info"].setText(movie_title)
+                        self["genre"].setText("")	
+			self.menulist.append(imdb_show(movie_title, self.no_image_poster, str(elapsed), "", search_title))
+		else:
+			got_genre = re.findall('"Genre":"(.*?)"', data)
+			genre = "(%s)" % got_genre[0]
+
+			### Downloading Cover mit file exist check ###
+			if not os.path.exists(path):
+				print "EMC iMDB: Download", poster_url[0]
+				urllib._urlopener = AppURLopener()
+				urllib.urlretrieve(poster_url[0], path)
+				urllib.urlcleanup()
+				if os.path.exists(path):
+					self["info"].setText(count)
+					self["m_info"].setText(movie_title)
+					self["genre"].setText(genre)
+					self.menulist.append(imdb_show(movie_title, path, str(elapsed), genre, search_title))
+			else:
+				self["info"].setText(count)
+				self["m_info"].setText(movie_title)
+				self["genre"].setText(genre)
+				self.menulist.append(imdb_show(movie_title, path, "Exist already", genre, search_title))
+
 		self["menulist"].l.setList(self.menulist)
 		self["menulist"].l.setItemHeight(24)
 		self.check = "true"
 		avg = self.t_elapsed / self.counter
-		print avg
-		done = "%s Filme in %s ms gefunden. Durchschnittsgeschwindigkeit: %.1f ms" % (str(self.counter), str(self.t_elapsed), int(avg))
+		done = "%s Filme in %s ms gefunden. Avg. Speed: %.1f ms" % (str(self.counter), str(self.t_elapsed), int(avg))
 		self["done_msg"].setText(done)
+		#self.showInfo()
 
 	def errorLoad(self, error):
 		print "Please report: %s" % str(error)
@@ -180,13 +194,27 @@ class imdbscan(Screen):
 		self.check = "false"
 		self.close()
 
+	def red(self):
+		if self.check == "true":
+			m_poster_path = self["menulist"].getCurrent()[0][1]
+			if os.path.exists(m_poster_path):				
+				os.system("rm '%s'" % (m_poster_path))
+				done = "%s removed." % m_poster_path
+				self["done_msg"].setText(done)
+				print m_poster_path
+
+	def ok(self):
+		print "ok"
+
 	### Cover resize ###
 	def poster_resize(self, poster_path):
+		print "resize:", poster_path
 		self["poster"].hide()
 		sc = AVSwitch().getFramebufferScale() # Maybe save during init
 		self.picload = ePicLoad()
 		self.picload.PictureData.get().append(self.showCoverCallback)
 		size = self["poster"].instance.size()
+		print "sizzze:", size
 		self.picload.setPara((size.width(), size.height(), sc[0], sc[1], False, 1, "#00000000")) # Background dynamically
 		self.picload.startDecode(poster_path)
 
