66cbc48b3e9f86484ca7e74c5bc1a1b4812a52c2
betonme <glaserfrank@gmail.com>
2013-02-13 10:47:55 +0100

    Update src/plugin.py
    Config: Updated audio and subtitle check. Please provide feedback

--- src/plugin.py
+++ src/plugin.py
@@ -295,12 +295,14 @@ config.EMC.list_skip_size            = ConfigSelectionNumber(3, 10, 1, default =
 config.EMC.InfoLong                  = ConfigSelection(choices = [("IMDbSearch", _("IMDb Search")), ("TMDBInfo", _("TMDB Info")), ('CSFDInfo', _('CSFD Info'))], default = "IMDbSearch")
 
 def checkList(cfg):
-	if cfg.value not in cfg.choices.choices:
-		if cfg.default in cfg.choices.choices:
+	for choices in cfg.choices.choices:
+		if cfg.value == choices[0]:
+			return
+	for choices in cfg.choices.choices:
+		if cfg.default == choices[0]:
 			cfg.value = cfg.default
-		else:
-			cfg.value = cfg.choices.choices[0]
-	print cfg.value
+			return
+	cfg.value = cfg.choices.choices[0][0]
 
 checkList(config.EMC.epglang)
 checkList(config.EMC.sublang1)
