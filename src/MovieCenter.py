4cdc23557be9ee02ded6d3ac0040ec5793485a22
Captain <captain.onboard@web.de>
2015-07-12 20:51:47 +0200

    add .stream extention for new IOTV record oe-a/pli images

--- src/MovieCenter.py
+++ src/MovieCenter.py
@@ -75,7 +75,7 @@ global sidDVB, sidDVD, sidMP3
 
 # Media types
 extAudio    = frozenset([".ac3", ".dts", ".flac", ".m4a", ".mp2", ".mp3", ".ogg", ".wav", ".wma", ".aac"])
-extVideo    = frozenset([".ts", ".trp", ".avi", ".divx", ".f4v", ".flv", ".img", ".ifo", ".iso", ".m2ts", ".m4v", ".mkv", ".mov", ".mp4", ".mpeg", ".mpg", ".mts", ".vob", ".wmv", ".bdmv", ".asf"])
+extVideo    = frozenset([".ts", ".trp", ".avi", ".divx", ".f4v", ".flv", ".img", ".ifo", ".iso", ".m2ts", ".m4v", ".mkv", ".mov", ".mp4", ".mpeg", ".mpg", ".mts", ".vob", ".wmv", ".bdmv", ".asf", ".stream"])
 extPlaylist = frozenset([".m3u", ".e2pls"])#, ".pls"])
 extMedia    = extAudio | extVideo | extPlaylist
 extDir      = frozenset([""])
