293625343436a2ab366a3c2ad0dd944079c7c4b3
dhwz <dhwz@gmx.net>
2017-12-30 17:27:29 +0100

    load the right files if original skin is enabled

--- src/MovieCenter.py
+++ src/MovieCenter.py
@@ -216,17 +216,23 @@ def getPosterPath(searchPath):
 #-------------------------------------------------
 def getNoPosterPath():
  if sz_w == 1920:
-	img = resolveFilename(SCOPE_CURRENT_SKIN, 'emc/no_poster.png')
-	if fileExists(img):
-		return img
-	else:
+	if config.EMC.use_orig_skin.value:
 		return "/usr/lib/enigma2/python/Plugins/Extensions/EnhancedMovieCenter/img_fhd/no_poster.png"
- else:
-	img = resolveFilename(SCOPE_CURRENT_SKIN, 'emc/no_poster.png')
-	if fileExists(img):
-		return img
 	else:
+		img = resolveFilename(SCOPE_CURRENT_SKIN, 'emc/no_poster.png')
+		if fileExists(img):
+			return img
+		else:
+			return "/usr/lib/enigma2/python/Plugins/Extensions/EnhancedMovieCenter/img_fhd/no_poster.png"
+ else:
+	if config.EMC.use_orig_skin.value:
 		return "/usr/lib/enigma2/python/Plugins/Extensions/EnhancedMovieCenter/img/no_poster.png"
+	else:
+		img = resolveFilename(SCOPE_CURRENT_SKIN, 'emc/no_poster.png')
+		if fileExists(img):
+			return img
+		else:
+			return "/usr/lib/enigma2/python/Plugins/Extensions/EnhancedMovieCenter/img/no_poster.png"
 
 #-------------------------------------------------
 # func: readBasicCfgFile( file )
@@ -1704,18 +1710,26 @@ class MovieCenterData(VlcPluginInterfaceList, PermanentSort, E2Bookmarks, EMCBoo
 def loadPix(name):
  if sz_w == 1920:
 	plugindir = '/usr/lib/enigma2/python/Plugins/Extensions/EnhancedMovieCenter/img_fhd/'
-	if fileExists(resolveFilename(SCOPE_CURRENT_SKIN, 'emc/'+name)):
-		pic = LoadPixmap(cached=True, path=resolveFilename(SCOPE_CURRENT_SKIN, 'emc/'+name))
-	else:
+	if config.EMC.use_orig_skin.value:
 		pic = LoadPixmap(cached=True, path=plugindir+name)
-	return pic
+		return pic
+	else:
+		if fileExists(resolveFilename(SCOPE_CURRENT_SKIN, 'emc/'+name)):
+			pic = LoadPixmap(cached=True, path=resolveFilename(SCOPE_CURRENT_SKIN, 'emc/'+name))
+		else:
+			pic = LoadPixmap(cached=True, path=plugindir+name)
+		return pic
  else:
 	plugindir = '/usr/lib/enigma2/python/Plugins/Extensions/EnhancedMovieCenter/img/'
-	if fileExists(resolveFilename(SCOPE_CURRENT_SKIN, 'emc/'+name)):
-		pic = LoadPixmap(cached=True, path=resolveFilename(SCOPE_CURRENT_SKIN, 'emc/'+name))
-	else:
+	if config.EMC.use_orig_skin.value:
 		pic = LoadPixmap(cached=True, path=plugindir+name)
-	return pic
+		return pic
+	else:
+		if fileExists(resolveFilename(SCOPE_CURRENT_SKIN, 'emc/'+name)):
+			pic = LoadPixmap(cached=True, path=resolveFilename(SCOPE_CURRENT_SKIN, 'emc/'+name))
+		else:
+			pic = LoadPixmap(cached=True, path=plugindir+name)
+		return pic
 
 moviecenter = None
 
