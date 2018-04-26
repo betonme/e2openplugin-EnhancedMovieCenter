98e88a587bda0fb09b6f513b2b12f43ba52ad936
dhwz <dhwz@gmx.net>
2018-04-26 18:32:45 +0200

    raised EMCVersion

--- src/EnhancedMovieCenter.py
+++ src/EnhancedMovieCenter.py
@@ -59,7 +59,7 @@ from EMCTasker import emcTasker, emcDebugOut
 
 sz_w = getDesktop(0).size().width()
 
-EMCVersion = "git20180420"
+EMCVersion = "git20180426"
 EMCAbout = "Enhanced Movie Center " +EMCVersion+ "\n\n(c) 2012-2018 by\nCoolman, betonme, Swiss-MAD & the many other volunteers."
 
 def setEPGLanguage(dummyself=None, dummy=None):
