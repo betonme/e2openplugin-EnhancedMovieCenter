90e151a9b81521ad5b9e3ba35db2c98926c66c61
dhwz <dhwz@gmx.net>
2018-06-11 23:16:39 +0200

    removed obsolete code from EMCClockToText converter
    locale translation is automatically done by image, no separated code is necessary

--- Components/Converter/EMCClockToText.py
+++ Components/Converter/EMCClockToText.py
@@ -1,9 +1,4 @@
-﻿#!/usr/bin/python
-# encoding: utf-8
-#
-# Copyright (C) 2011 by Coolman & betonme
-
-from Converter import Converter
+﻿from Converter import Converter
 from time import localtime, strftime, gmtime
 from Components.Element import cached
 from Components.config import config
@@ -59,98 +54,15 @@ class EMCClockToText(Converter, object):
 		elif self.type == self.DEFAULT:
 			return "%02d:%02d" % (t.tm_hour, t.tm_min)
 		elif self.type == self.DATE:
-			CoolString = "%A %B %d, %Y"
-			if config.osd.language.value == "de_DE":
-				CoolString = "%A, %d. %B %Y"
-				t2 = ["Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag","Sonntag"][t.tm_wday]
-				m2 = ["Januar","Februar",u"M\xe4rz","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember"][t.tm_mon - 1]
-				CoolString = CoolString.replace('%A', t2)
-				CoolString = CoolString.replace('%B', m2)
-			return strftime(CoolString, t)
+			return strftime("%A %B %d, %Y", t)
 		elif self.type == self.FORMAT:
-			pos = self.fmt_string.find('%')
-			apos = self.fmt_string.find('%a')
-			Apos = self.fmt_string.find('%A')
-			bpos = self.fmt_string.find('%b')
-			Bpos = self.fmt_string.find('%B')
-			if pos > 0:
-				s1 = self.fmt_string[:pos]
-				s2 = strftime(self.fmt_string[pos:], t)
-				CoolString = str(s1+s2)
-				if config.osd.language.value == "de_DE":
-					if apos > -1:
-						CoolString = CoolString.replace('Mon', 'Mo')
-						CoolString = CoolString.replace('Tue', 'Di')
-						CoolString = CoolString.replace('Wed', 'Mi')
-						CoolString = CoolString.replace('Thu', 'Do')
-						CoolString = CoolString.replace('Fri', 'Fr')
-						CoolString = CoolString.replace('Sat', 'Sa')
-						CoolString = CoolString.replace('Sun', 'So')
-					if Apos > -1:
-						CoolString = CoolString.replace('Monday', 'Montag')
-						CoolString = CoolString.replace('Tuesday', 'Dienstag')
-						CoolString = CoolString.replace('Wednesday', 'Mittwoch')
-						CoolString = CoolString.replace('Thursday', 'Donnerstag')
-						CoolString = CoolString.replace('Friday', 'Freitag')
-						CoolString = CoolString.replace('Saturday', 'Samstag')
-						CoolString = CoolString.replace('Sunday', 'Sonntag')
-					if bpos > -1:
-						CoolString = CoolString.replace('Mar', 'Mrz')
-						CoolString = CoolString.replace('May', 'Mai')
-						CoolString = CoolString.replace('June', 'Jun')
-						CoolString = CoolString.replace('July', 'Jul')
-						CoolString = CoolString.replace('Sept', 'Sep')
-						CoolString = CoolString.replace('Oct', 'Okt')
-						CoolString = CoolString.replace('Dec', 'Dez')
-					if Bpos > -1:
-						CoolString = CoolString.replace('January', 'Januar')
-						CoolString = CoolString.replace('February', 'Februar')
-						CoolString = CoolString.replace('March', 'März')
-						CoolString = CoolString.replace('April', 'April')
-						CoolString = CoolString.replace('May', 'Mai')
-						CoolString = CoolString.replace('June', 'Juni')
-						CoolString = CoolString.replace('July', 'Juli')
-						CoolString = CoolString.replace('October', 'Oktober')
-						CoolString = CoolString.replace('December', 'Dezember')
-				return CoolString
+			spos = self.fmt_string.find('%')
+			if spos >-1:
+				s1 = self.fmt_string[:spos]
+				s2 = strftime(self.fmt_string[spos:], t)
+				return str(s1+s2)
 			else:
-				CoolString = strftime(self.fmt_string, t)
-				if config.osd.language.value == "de_DE":
-					if apos > -1:
-						CoolString = CoolString.replace('Mon', 'Mo')
-						CoolString = CoolString.replace('Tue', 'Di')
-						CoolString = CoolString.replace('Wed', 'Mi')
-						CoolString = CoolString.replace('Thu', 'Do')
-						CoolString = CoolString.replace('Fri', 'Fr')
-						CoolString = CoolString.replace('Sat', 'Sa')
-						CoolString = CoolString.replace('Sun', 'So')
-					if Apos > -1:
-						CoolString = CoolString.replace('Monday', 'Montag')
-						CoolString = CoolString.replace('Tuesday', 'Dienstag')
-						CoolString = CoolString.replace('Wednesday', 'Mittwoch')
-						CoolString = CoolString.replace('Thursday', 'Donnerstag')
-						CoolString = CoolString.replace('Friday', 'Freitag')
-						CoolString = CoolString.replace('Saturday', 'Samstag')
-						CoolString = CoolString.replace('Sunday', 'Sonntag')
-					if bpos > -1:
-						CoolString = CoolString.replace('Mar', 'Mrz')
-						CoolString = CoolString.replace('May', 'Mai')
-						CoolString = CoolString.replace('June', 'Jun')
-						CoolString = CoolString.replace('July', 'Jul')
-						CoolString = CoolString.replace('Sept', 'Sep')
-						CoolString = CoolString.replace('Oct', 'Okt')
-						CoolString = CoolString.replace('Dec', 'Dez')
-					if Bpos > -1:
-						CoolString = CoolString.replace('January', 'Januar')
-						CoolString = CoolString.replace('February', 'Februar')
-						CoolString = CoolString.replace('March', 'März')
-						CoolString = CoolString.replace('April', 'April')
-						CoolString = CoolString.replace('May', 'Mai')
-						CoolString = CoolString.replace('June', 'Juni')
-						CoolString = CoolString.replace('July', 'Juli')
-						CoolString = CoolString.replace('October', 'Oktober')
-						CoolString = CoolString.replace('December', 'Dezember')
-				return CoolString
+				return strftime(self.fmt_string, t)
 		else:
 			return "???"
 
