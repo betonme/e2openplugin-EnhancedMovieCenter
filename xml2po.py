1e28fa731805acfbe27bffce876f2f2a251dbefd
Captain <captain.onboard@web.de>
2020-11-14 20:52:44 +0100

    xml2po.py fix build with ubunto 20_04.1

--- xml2po.py
+++ xml2po.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
 import sys
 import os
 import string
@@ -51,14 +53,14 @@ for arg in sys.argv[1:]:
 	attrlist.sort(key=lambda a: a[0])
 
 	for (k,c) in attrlist:
-		print
-		print '#: ' + arg
+		print('')
+		print('#: ' + arg)
 		string.replace(k, "\\n", "\"\n\"")
 		if c:
 			for l in c.split('\n'):
-				print "#. ", l
+				print("#. ", l)
 		if str(k).strip() != "":
-			print 'msgid "' + str(k) + '"'
-			print 'msgstr ""'
+			print('msgid "' + str(k) + '"')
+			print('msgstr ""')
 
 	attrlist = set()
